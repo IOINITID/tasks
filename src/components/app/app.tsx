ec2609b872368d468f1d65ab664478464eac6e6f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-20 23:00:56 +0300

    Updated app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import Task from "../task/task";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
       <Task id="1" value="Some task..." complete={false} />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+          placeholder="Add some task..."
+        />
+      </View>
     </View>
   );
 };
@@ -18,6 +29,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputContainer: {
+    width: "100%",
+  },
+  textInput: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
   },
 });
 
