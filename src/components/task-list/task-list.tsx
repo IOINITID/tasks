cdd2c6d599c2dd24db6f3d90f9cf40bae791fb90
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-22 22:07:24 +0300

    Added task-list component

---
+++ src/components/task-list/task-list.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { ITask } from "../../features/tasks/tasksSlice";
+import Task from "../task/task";
+
+interface ITaskList {
+  tasks: ITask[];
+}
+
+const TaskList = ({ tasks }: ITaskList) => {
+  return (
+    <ScrollView style={styles.container}>
+      {tasks.map((task) => {
+        return (
+          <Task
+            key={task.id}
+            id={task.id}
+            value={task.value}
+            complete={task.complete}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+  },
+});
+
+export default TaskList;
